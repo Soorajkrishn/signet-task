b5e2f0df7bcfd1bcb975a15667e17b7aeef16cc5
Soorajkrishn <108404141+Soorajkrishn@users.noreply.github.com>
2022-09-22 12:17:58 +0530

    Custom Video Player

--- src/Pages/Chats/Chats.js
+++ src/Pages/Chats/Chats.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import APIUrlConstants from '../../Config/APIUrlConstants';
 import { fetchCall } from '../../Services/APIService';
@@ -13,9 +13,15 @@ import useAnalyticsEventTracker from '../../Hooks/useAnalyticsEventTracker';
 export default function Chats() {
   const location = useLocation();
   const [snackBar, setSnackBar] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [size, setSize] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [progress, setProgress] = useState(0);
   const [isLoading, setIsLoading] = useState(location.state != null ? location.state.showLoader : true);
   const roledId = localStorage.getItem('roleId');
   const { buttonTracker } = useAnalyticsEventTracker();
+  const videoRef = useRef(null);
+
   useEffect(() => {
     if (roledId === userRoleId.remoteSmartUser) {
       preSetupFCWidget(true).then();
@@ -38,13 +44,63 @@ export default function Chats() {
     },
     [],
   );
-  /* Component cleanup function call end  */
+
+
+  const vid = document.getElementById("demoVideo");
+  const videoLength = vid?.duration
+
   const handleClick = async () => {
     const [statusCode] = await fetchCall(APIUrlConstants.CONTACT_SALES + '/' + localStorage.getItem('id'), apiMethods.POST, {});
     if (statusCode === httpStatusCode.SUCCESS) {
       setSnackBar(true);
       localStorage.setItem('contactSales', 'true');
     }
+  };
+
+  const minutes = parseInt(videoLength / 60, 10);
+  const seconds = parseInt(videoLength % 60, 10);
+
+  const time = minutes + ':' + seconds
+
+  const videoHandler = (control) => {
+    if (control === "play") {
+      videoRef.current.play();
+      setPlaying(true);
+
+    } else if (control === "pause") {
+      videoRef.current.pause();
+      setPlaying(false);
+
+    }
+  };
+
+
+  const fastForward = () => {
+    videoRef.current.currentTime += 5;
+  };
+
+  const revert = () => {
+    videoRef.current.currentTime -= 5;
+  };
+
+  setInterval(() => {
+    setCurrentTime(videoRef.current?.currentTime);
+    setProgress((videoRef.current.currentTime / videoLength) * 100);
+  }, 1000);
+
+  const setVolume = (volumeValue) => {
+    videoRef.current.volume = (volumeValue / 100);
+  }
+
+  useEffect(() => {
+    if (currentTime === videoLength) {
+      setPlaying(false);
+    }
+  },
+    [currentTime]
+  )
+  const setPlayBack = (speed) => {
+    videoRef.current.playbackRate = speed;
   };
 
   return (
@@ -60,11 +116,69 @@ export default function Chats() {
             </div>
           </div>
           <div className="wrapperCard--body">
-            <div className="videoWrapper">
-              <video width="100%" height="100%" controls>
+            <div className={size ? 'fullScreen' : 'videoWrapper'}>
+              <video className='videoSize' onCanPlay={setPlayBack} ref={videoRef} id="demoVideo">
                 <source src="https://signet-group-public.s3.amazonaws.com/Animate+-+22182.mp4" />
               </video>
+              <div className='controlWrapper'>
+                <p style={{ color: 'white', fontSize: '36px' }}>Chat Demo</p>
+              </div>
+              <div className={size ? 'fullScreen_bottomControl' : 'bottomControl'}>
+
+                <div>
+                  <input style={{ width: size ? '95%' : "657.61px" }} type="range" min='0' max='100' step='1' value={progress === 'NaN:NaN' ? '0' : progress} />
+                </div>
+
+                <div className='row control'>
+                  <div className='col-4'>
+                    <img className='volumeIcon' src="/images/video/volume-icon.svg" alt='volume' />
+                    <div className='volumeBar'>
+                      <input id="vol-control" type="range" min="0" max="100" step="1" onInput={(e) => setVolume(e.target.value)} onChange={(e) => setVolume(e.target.value)} />
+                    </div>
+                  </div>
+                  <div className='col-6'>
+                    <div className='row'>
+                      <div className='col-2'>
+                        <p style={{ color: 'white' }}>
+                          {Math.floor(currentTime / 60) + ":" + ("0" + Math.floor(currentTime % 60)).slice(-2)}
+                        </p>
+                      </div>
+                      <div className='col-7'>
+                        <div className="controle">
+                          <img onClick={revert} className="backword" alt="backward" src="/images/video/icons8-wrewind.png" />
+                          {playing ? (
+                            <img onClick={() => videoHandler("pause")} className="controlsIcon--small" alt="pause" src="/images/video/icons8-wpause.png" />
+                          ) : (
+                            <img onClick={() => videoHandler("play")} className="controlsIcon--small" alt="play" src="/images/video/icons8-wplay.png" />
+                          )}
+                          <img onClick={fastForward} className="forward" alt="forward" src="/images/video/icons8-wfastforward.png" />
+                        </div>
+                      </div>
+                      <div className='col-2'>
+                        <p style={{ color: 'white' }}>{time === 'NaN:NaN' ? '00:00' : time}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-2'>
+                    <div >
+                      <div className='setting'>
+                        <img src="/images/video/settings.svg" alt='setting' />
+                        <div className="dropup-content">
+                          <Button className='speedSetting' variant='link' onClick={() => setPlayBack(0.5)}>0.5</Button>
+                          <Button className='speedSetting' variant='link' onClick={() => setPlayBack(1)}>1</Button>
+                          <Button className='speedSetting' variant='link' onClick={() => setPlayBack(1.5)}>1.5</Button>
+                          <Button className='speedSetting' variant='link' onClick={() => setPlayBack(2)}>2</Button>
+                        </div>
+                      </div>
+
+                      <img onClick={() => size ? setSize(false) : setSize(true)} alt='fullscreen' src="/images/video/icons8-full-screen.png" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             </div>
+
             {!snackBar && (
               <Button
                 className="buttonPrimary mb-5"
